bc38998cd851ee78e1c215b4d063c998c704e431
leaemery <lea.audrey.emery@gmail.com>
2021-01-20 12:11:51 +0100

    Initial commit

---
+++ data/Chantiers_2006_17.js
@@ -0,0 +1,12 @@
+var json_Chantiers_2006_17 = {
+"type": "FeatureCollection",
+"name": "Chantiers_2006_17",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Regions": "Hauts de France", "Departemen": "Oise", "Lieux chan": "Avricourt", "CP": "60310", "statut": "commune", "theme": "Patrimoine", "Annee": "2006" }, "geometry": { "type": "Point", "coordinates": [ 2.862205984152129, 49.656002557852858 ] } },
+{ "type": "Feature", "properties": { "Regions": "Hauts de France", "Departemen": "Somme", "Lieux chan": "Cardonnette", "CP": "80260", "statut": "commune", "theme": "Patrimoine", "Annee": "2006" }, "geometry": { "type": "Point", "coordinates": [ 2.358072199510729, 49.951247536562541 ] } },
+{ "type": "Feature", "properties": { "Regions": "Hauts de France", "Departemen": "Somme", "Lieux chan": "Demuin", "CP": "80110", "statut": "commune", "theme": "Patrimoine", "Annee": "2006" }, "geometry": { "type": "Point", "coordinates": [ 2.483033851616859, 49.772162572363492 ] } },
+{ "type": "Feature", "properties": { "Regions": "Hauts de France", "Departemen": "Nord", "Lieux chan": "Rumegies", "CP": "59226", "statut": "commune", "theme": "Patrimoine", "Annee": "2006" }, "geometry": { "type": "Point", "coordinates": [ 3.35255554689726, 50.489435727417025 ] } },
+{ "type": "Feature", "properties": { "Regions": "Hauts de France", "Departemen": "Aisne", "Lieux chan": "Saint Erme Outre et Ramecourt", "CP": "2820", "statut": "commune", "theme": "Patrimoine", "Annee": "2006" }, "geometry": { "type": "Point", "coordinates": [ 3.838773955309213, 49.514128654089475 ] } }
+]
+}
